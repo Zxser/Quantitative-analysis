9ff801a7a45656c910f8e8886ac99adac0545b56
hugo2046 <shen.lan123@gmail.com>
2022-06-12 22:52:33 +0800

    update func

---
+++ C-择时类/”Trader-Company“集成算法交易策略/参考/tradercompany/tradercompany/binary_operators.py
@@ -0,0 +1,31 @@
+'''
+Author: hugo2046 shen.lan123@gmail.com
+Date: 2022-06-12 21:10:12
+LastEditors: hugo2046 shen.lan123@gmail.com
+LastEditTime: 2022-06-12 22:23:13
+Description: 符号系统
+'''
+from operator import add
+from operator import mul as multiple
+from operator import sub as diff
+
+# def add(x, y):
+#     return x + y
+
+# def diff(x, y):
+#     return x - y
+
+# def multiple(x, y):
+#     return x * y
+
+
+def get_x(x, y):
+    return x
+
+
+def get_y(x, y):
+    return y
+
+
+def x_is_greater_than_y(x, y):
+    return (x > y) * 1.0
